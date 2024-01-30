c490614cf93b88afd935acc68743f857e458a914
Tom JD1 Xue <xuejd1@lenovo.com>
2024-01-30 11:06:26 +0800

    workable demo

---
+++ PopupDemo/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using static System.Net.Mime.MediaTypeNames;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PopupDemo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            popup.IsOpen = !(popup.IsOpen);
+        }
+
+        private async void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            var messageDialog = new MessageDialog("Hello");
+            await messageDialog.ShowAsync();
+        }
+    }
+}
